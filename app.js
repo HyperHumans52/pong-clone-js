7eae6bc3784e94cab5f8ef4c309fdd0465e10087
HyperHumans52 <webguru72@gmail.com>
2025-10-09 15:43:15 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+Game:
+
+Description: Using JavaScript and HTML5 Canvas to create a 2D game environment.
+
+Features:
+
+Tasks:
+
+*/
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
