f50706960c6cefc6898318c43b418d2967e00578
HyperHumans52 <webguru72@gmail.com>
2025-10-10 03:41:26 -0400

    Minor file updates

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-/*
-Game:
-
-Description: Using JavaScript and HTML5 Canvas to create a 2D game environment.
-
-Features:
-
-Tasks:
-
-*/
-
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
