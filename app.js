d778c3f7c67e2e71af8053cc2c28bc77da9c127b
HyperHumans52 <webguru72@gmail.com>
2025-10-16 14:46:48 -0400

    Added sounds and custom 8-bit number images for score

--- app.js
+++ app.js
@@ -7,14 +7,48 @@ const paddleSpeed = 5; // Vertical speed (pixels per frame)
 const goalPadding = 30; // Space between goal and paddle
 const ballSize = 10; // Width and height of the ball
 const ballSpeed = 5; // Velocity of the ball (pixels per frame)
-const winningScore = 2;
+const winningScore = 10;
+const wallHitSound = new Audio('assets/audio/wall.mp3');
+const paddleHitSound = new Audio('assets/audio/paddle.mp3');
+const goalSound = new Audio('assets/audio/goal.mp3');
 
 let player1 = { x: goalPadding, y: canvas.height / 2 - paddleHeight / 2 }; // Starting position of player 1
 let player2 = { x: canvas.width - (goalPadding + paddleWidth), y: canvas.height / 2 - paddleHeight / 2 }; // Starting position of player 2
 let ball = { x: canvas.width / 2, y: canvas.height / 2, dx: ballSpeed, dy: ballSpeed }; // Starting position and velocity of the ball
 let player1Score = 0;
 let player2Score = 0;
-let gameRunning = true;
+let gameRunning = false;
+let canPlay = false;
+
+// Set up/preload images for scoring
+const imageFiles = Array.from({ length: 10 }, (_, i) => i + '.png'); // Array of image file names
+const images = await Promise.all(
+    imageFiles.map(
+        (file) =>
+            new Promise((resolve, reject) => {
+                const img = new Image();
+                img.onload = () => resolve(img);
+                img.onerror = reject;
+                img.src = 'assets/images/' + file;
+            })
+    )
+);
+
+// Preload audio
+const audioFiles = ['assets/audio/wall.mp3', 'assets/audio/paddle.mp3', 'assets/audio/goal.mp3'];
+const audioPromises = audioFiles.map(
+    (file) =>
+        new Promise((resolve, reject) => {
+            const audio = new Audio(file);
+            audio.oncanplaythrough = () => resolve(audio);
+            audio.onerror = reject;
+        })
+);
+const sounds = await Promise.all(audioPromises);
+
+if (images.length === imageFiles.length && sounds.length === audioFiles.length) {
+    canPlay = true;
+}
 
 const keys = {}; // Object to store pressed keys
 document.addEventListener('keydown', (e) => {
@@ -53,30 +87,42 @@ function drawGame() {
     ctx.fillRect(ball.x - ballSize / 2, ball.y - ballSize / 2, ballSize, ballSize); // Implement squared ball
 
     // Display scores
-    ctx.fillStyle = '#fff';
-    ctx.font = '72px Courier New';
-    ctx.fillText(player1Score, canvas.width / 2 - (player1Score < 10 ? 80 : 120), 100); // Adjust position for single-digit scores
-    ctx.fillText(player2Score, canvas.width / 2 + 40, 100);
+    const score1Digits = player1Score.toString().split('').reverse();
+    const score2Digits = player2Score.toString().split('');
+    score1Digits.forEach((digit, index) => {
+        ctx.drawImage(images[digit], canvas.width / 2 - 64 - 40 * index, 14);
+    });
+    score2Digits.forEach((digit, index) => {
+        ctx.drawImage(images[digit], canvas.width / 2 + 32 + 40 * index, 14);
+    });
 
     // Check if game is over and display "Game Over" message
     if (!gameRunning) {
-        ctx.fillStyle = '#fff';
-        ctx.font = '72px Courier New';
-        ctx.fillText('Game Over', canvas.width / 2 - 195, canvas.height / 2);
-        ctx.font = '24px Courier New';
-        ctx.fillText('Press any key to restart', canvas.width / 2 - 170, canvas.height / 2 + 36);
+        if (player1Score === winningScore || player2Score === winningScore) {
+            // Game over
+            ctx.fillStyle = '#fff';
+            ctx.font = '72px Courier New';
+            ctx.fillText('Game Over', canvas.width / 2 - 195, canvas.height / 2);
+            ctx.font = '24px Courier New';
+            ctx.fillText('Press any key to restart', canvas.width / 2 - 170, canvas.height / 2 + 36);
+        } else {
+            // Game start
+            ctx.fillStyle = '#fff';
+            ctx.font = '24px Courier New';
+            ctx.fillText('Press any key to start', canvas.width / 2 - 160, canvas.height / 2 - 36);
+        }
     }
 }
 
 function updateGame() {
     // Player 1 movement
-    // if ('w' in keys && player1.y > 0) {
-    //     player1.y -= paddleSpeed;
-    // }
-    // if ('s' in keys && player1.y < canvas.height - paddleHeight) {
-    //     player1.y += paddleSpeed;
-    // }
-    aiPlayerControl(player1);
+    if ('w' in keys && player1.y > 0) {
+        player1.y -= paddleSpeed;
+    }
+    if ('s' in keys && player1.y < canvas.height - paddleHeight) {
+        player1.y += paddleSpeed;
+    }
+    // aiPlayerControl(player1);
     // Player 2 movement
     // if ('ArrowUp' in keys && player2.y > 0) {
     //     player2.y -= paddleSpeed;
@@ -91,7 +137,8 @@ function updateGame() {
     ball.y += ball.dy;
 
     // Check for collision with top or bottom wall
-    if (ball.y - ballSize / 2 < 0 || ball.y + ballSize / 2 > canvas.height) {
+    if (ball.y - ballSize / 2 <= 0 || ball.y + ballSize / 2 >= canvas.height) {
+        wallHitSound.play();
         ball.dy = -ball.dy;
     }
 
@@ -132,12 +179,14 @@ function updateGame() {
 function handleCollision(player) {
     const paddleCenter = player.y + paddleHeight / 2;
     const distanceFromCenter = paddleCenter - ball.y;
+    paddleHitSound.play();
     ball.x = player.x < canvas.width / 2 ? player1.x + paddleWidth + ballSize / 2 : player.x - ballSize / 2; // Reposition the ball to the right of the paddle to avoid trapping the ball in the paddle
     ball.dx = -ball.dx;
     ball.dy -= 0.1 * distanceFromCenter; // Adjust the ball's velocity based on the distance from the paddle's center
 }
 
 function updateScore(player) {
+    goalSound.play();
     if (player === 1) player1Score++;
     else player2Score++;
     resetBall(player === 1 ? 2 : 1);
@@ -146,7 +195,7 @@ function updateScore(player) {
 function resetBall(player) {
     // Reset position
     ball.x = canvas.width / 2;
-    ball.y = Math.random() * canvas.height;
+    ball.y = Math.random() * (canvas.height - 20) + 10;
     // Reset velocity
     ball.dx = player === 1 ? -ballSpeed : ballSpeed;
     ball.dy = ballSpeed * (Math.random() < 0.5 ? -1 : 1);
@@ -184,7 +233,7 @@ function aiPlayerControl(player) {
     }
 }
 
-drawGame(); // Initial draw
+if (canPlay) drawGame(); // Initial draw
 
 function gameLoop() {
     if (!gameRunning) return;
